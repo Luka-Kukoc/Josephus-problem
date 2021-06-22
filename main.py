c096f1f128ee7e8aa8f83527f99d9c7b9d5a5c26
autos-ismos <69589847+autos-ismos@users.noreply.github.com>
2021-06-22 12:56:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import math
 x = int(input("How many people are there? "))
-i = 1
-lst = []
-while i < x:
-    i *= 2
-    lst.append(i)
-b = lst[-2]
-a = x - b
-winner = str(1 + 2*a)
-print("Josephus should be at position:" + winner)
+if math.log2(x).is_integer():
+    print("Josephus should be at position: 1")
+else:
+    i = 1
+    lst = []
+    while i < x:
+        i *= 2
+        lst.append(i)
+    b = lst[-2]
+    winner = str(1 + 2*(x-b))
+    print("Josephus should be at position:" + winner)
